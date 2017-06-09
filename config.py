0a4cd8c9a1df3435277ad6f0efbb6d29c3521622
Navjot Singh <weavebytes@gmail.com>
2017-06-09 20:43:53 +0530

    config file for proj

---
+++ config.py
@@ -0,0 +1,17 @@
+#
+# config/settings for scrapper
+#
+
+DB_HOST = '127.0.0.1'
+DB_PORT = 27017
+
+# authorization database used to authenticate username/password
+AUTH_DB_NAME = 'admin'
+
+DB_NAME = 'carrermaker'
+DB_USER = 'admin'
+DB_PASS = '123'
+
+# scrap types
+# ST_DRY_CLEANERS = 'DRY-CLEANERS'
+# ST_SHOE_REPAIR = 'SHOE-REPAIR'
